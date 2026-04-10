399838104cd334327f07868341a247b4f1dc98b7
vlerOux <vleroux9738@gmail.com>
2026-04-10 16:59:24 +0200

    fix: update pytest configuration and adjust Python path in test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -4,6 +4,10 @@ import os
 import pytest
 from pathlib import Path
 from dotenv import load_dotenv
+import sys
+
+# Ajoute la racine du projet au path Python
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 load_dotenv()
 
